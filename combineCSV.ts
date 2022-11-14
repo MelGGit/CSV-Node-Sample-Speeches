79ed6899ee4d7deeec4c6279f99e580b2df30a8e
Mel Grosser <grosser.mel@live.de>
2022-11-14 17:02:25 +0100

    fix error

--- combineCSV.ts
+++ combineCSV.ts
@@ -5,7 +5,7 @@ import { parse } from 'csv-parse/sync'
 export default async function combineCSV (totalDownloadedCSV: number): Promise<SpeechType[]> {
   let result:SpeechType[] = []
   for(let i = 0; i <= totalDownloadedCSV -1 ;i++) {
-    const fileContent = await fs.readFile(`files/file${0}.csv`)    
+    const fileContent = await fs.readFile(`files/file${i}.csv`)    
     const records: SpeechType[] = await parse(fileContent, {
       columns: true,
       trim: true,
